61e972dbf70d6899bfe2f1858ad3ef4bf439fd8a
kkairouz <KM.Kairouz@gmail.com>
2026-05-04 01:46:52 -0400

    applied transformer 200 x 200 resize

--- model.py
+++ model.py
@@ -2,10 +2,13 @@ import torch
 import torchvision 
 
 device = torch.device(type="cuda")
+transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), torchvision.transforms.Resize((200,200))])
 
-training_data = torchvision.datasets.ImageFolder(root="archive/chest_xray/train")
 
-testing_data = torchvision.datasets.ImageFolder(root="archive/chest_xray/test") 
+training_data = torchvision.datasets.ImageFolder(root="archive/chest_xray/train", transform=transform)
+testing_data = torchvision.datasets.ImageFolder(root="archive/chest_xray/test",transform=transform)  
+
+print(training_data[0][0].shape)
 
 class CNN(torch.nn.Module): 
 
@@ -13,6 +16,7 @@ class CNN(torch.nn.Module):
         super().__init__() 
         self.nn = torch.nn.Sequential(
 
+            
 
 
 
